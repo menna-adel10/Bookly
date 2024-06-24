2bab69294f9349a9fcc60c6eb2f76637192d92f1
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-06-25 00:55:04 +0300

    create style file and add tittle view

--- lib/Features/home/persentation/views/widgets/Home_view_body.dart
+++ lib/Features/home/persentation/views/widgets/Home_view_body.dart
@@ -1,20 +1,31 @@
-import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'book_list_view.dart';
 import 'customAppBar.dart';
-import 'custom_list_View_Item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children:[
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 8.0),
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children:[
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left:8.0),
+            child: Text('Best Seller',
+              style: Styles.tittleMedium,),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+abstract class Styles
+{
+  static const tittleMedium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+}
