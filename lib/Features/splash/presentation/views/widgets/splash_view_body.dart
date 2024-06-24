a0e20027f96a660eabe71801cbbb784430690451
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-06-24 12:30:33 +0300

    create splash view body

---
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
