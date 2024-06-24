3e8f462a2f856798716444272dd504cbe1c600f7
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-06-25 02:09:36 +0300

    best seller list View item part 1

--- lib/Features/home/persentation/views/widgets/Home_view_body.dart
+++ lib/Features/home/persentation/views/widgets/Home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,19 +11,21 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 8.0),
-      child:  Column(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
+        children: [
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(
             height: 50,
           ),
           Padding(
-            padding: EdgeInsets.only(left:8.0),
-            child: Text('Best Seller',
-              style: Styles.tittleMedium,),
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(
+              'Best Seller',
+              style: Styles.tittleMedium,
+            ),
           )
         ],
       ),
@@ -30,7 +33,32 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
+class BestSellerList extends StatelessWidget {
+  const BestSellerList({super.key});
 
-
-
-
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.8 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: const DecorationImage(
+                    image: AssetImage(
+                  AssetsData.testImage,
+                )),
+              ),
+            ),
+          ),
+          const Column(
+            children: [],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart.';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BooklyApp()); 
@@ -17,7 +18,12 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratAlternatesTextTheme(
+          ThemeData.dark().textTheme,
+        ),
+      ),
       home: const SplashView(),
     );
   }
