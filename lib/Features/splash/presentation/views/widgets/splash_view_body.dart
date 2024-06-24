5d90c6c7fd10b0269eb9dfe9fd0830020d8bfcfd
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-06-24 14:47:12 +0300

    adding assets

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -3,8 +3,37 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  //adelo range elana 3ayzo
+  late Animation<Offset> slidingAnimation;
+  
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 100)
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: Offset(0, 2), end: Offset.zero).animate(
+            animationController);
+    animationController.forward();
+
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +42,25 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text('Read free books',
-        textAlign: TextAlign.center,
-        )
+        buildAnimatedBuilder(),
 
 
       ],
     );
+  }
+
+  AnimatedBuilder buildAnimatedBuilder() {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context,_) {
+          return
+          SlideTransition(
+            position: slidingAnimation,
+
+            child: const Text('Read free books',
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
   }
 }
