606615acc62f3f2c9c6e683a46e64cd1638c7b5a
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-06-26 13:59:25 +0300

    finished home repo implementation

--- lib/Features/home/data/repos/home_repo_implementation.dart
+++ lib/Features/home/data/repos/home_repo_implementation.dart
@@ -31,9 +31,23 @@ class HomeRepoImp implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
 
+    try {
+      var data = await apiServices.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+
+  }
 }
