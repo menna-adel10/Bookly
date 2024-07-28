3d27dc56253e1648d6becc1f7921469ff64f2069
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2024-07-28 14:36:37 +0300

    add Either class

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity(this.title, this.authorName, this.image, this.rating,
+      this.price) ;
+}
